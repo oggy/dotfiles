48b02a6fed9ede4761be093f1541c64d5cb1dcf6
George Ogata <george.ogata@gmail.com>
2013-01-11 15:03:11 -0500

    Python helpers, python history.

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,4 +1,7 @@
-export PATH=/Users/g/brew/share/python:$PATH
+export PATH=~/brew/share/python:$PATH
+export PYTHONSTARTUP=~/.pythonrc
+
+alias cdpy='cd `python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`'
 
 function pyclean {
     local root=.
