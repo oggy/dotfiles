ee51dbf7442c9ef5d99174b8361fb3708768d322
George Ogata <george.ogata@gmail.com>
2015-08-02 00:14:26 -0400

    Use atom for looksee editor.

--- .bashrc.d/ruby.sh
+++ .bashrc.d/ruby.sh
@@ -3,7 +3,7 @@ export RUBY_GC_HEAP_INIT_SLOTS=600000
 export RUBY_GC_MALLOC_LIMIT=59000000
 export RUBY_HEAP_FREE_MIN=100000
 
-export LOOKSEE_EDITOR="$EDITOR +%l %f"
+export LOOKSEE_EDITOR="atom %f:%l"
 export SNAILGUN_SHELL_OPTS="-l"
 
 alias gemls='gem list -rd --no-update-sources | less'
