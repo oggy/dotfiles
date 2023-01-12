136af855faf438555d8ebf5db24081b11b3ffc72
George Ogata <george.ogata@gmail.com>
2023-01-12 02:14:04 -0500

    Call virtualenv things venv
    Shorter, and seems more conventional these days.

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -10,28 +10,28 @@ pyclean() {
     find "$root" -name '*.pyc' -exec rm -f '{}' ';'
 }
 
-export PROMPT_COMMAND="virtualenv-auto-activate; $PROMPT_COMMAND"
+export PROMPT_COMMAND="venv-auto-activate; $PROMPT_COMMAND"
 export VIRTUALENV_ROOT=
 
-virtualenv-auto-activate() {
-    local root=`virtualenv-find-root`
+venv-auto-activate() {
+    local root=`venv-find-root`
     if [ "$root" != "$VIRTUALENV_ROOT" ]; then
         if [ -n "$VIRTUALENV_ROOT" ]; then
             VIRTUALENV_ROOT=
             deactivate
         else
             VIRTUALENV_ROOT="$root"
-            if [ -e "$root/virtualenv/bin/activate" ]; then
-                source "$root/virtualenv/bin/activate"
+            if [ -e "$root/venv/bin/activate" ]; then
+                source "$root/venv/bin/activate"
             fi
         fi
     fi
 }
 
-virtualenv-find-root() {
+venv-find-root() {
     local dir=`pwd`
     while [ -n "$dir" ]; do
-        if [ -e "$dir/virtualenv/bin/activate" ]; then
+        if [ -e "$dir/venv/bin/activate" ]; then
             echo -n "$dir"
             return 0
         fi
