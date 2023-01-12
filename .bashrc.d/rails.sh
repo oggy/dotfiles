be2d790ad8770a95352e4c2eb4a888ddb729c8cf
George Ogata <george.ogata@gmail.com>
2023-01-12 02:19:27 -0500

    Remove support for ancient rails versions

--- .bashrc.d/rails.sh
+++ .bashrc.d/rails.sh
@@ -1,19 +1,4 @@
 #
-# Directory to edge rails source.
-#
-RAILS_SRC=~/src/rails
-
-rails_framework() {
-    if [ -e bin/rails ]; then
-        echo bin/rails
-    elif [ -e config/apps.rb ]; then
-        echo -n padrino
-    else
-        echo -n rails
-    fi
-}
-
-#
 # Print the $RAILS_ENV or 'development' if it's undefined.
 #
 rails_env() {
@@ -24,67 +9,19 @@ rails_env() {
     fi
 }
 
-#
-# Run the appropriate rake command with the given arguments.
-#
-rails_rake() {
-    if [ -e bin/rake ]; then
-        bin/rake --trace "$@"
-    else
-        bundle exec rake --trace "$@"
-    fi
-}
-
-#
-# Start the rails server.
-#
-rails_server() {
-    local framework=$(rails_framework)
-    local options
-    local handler
-
-    if grep -q 'thin\b' Gemfile.lock; then
-        echo "Using thin." >&2
-        handler=thin
-    fi
-
-    if [ "$framework" = padrino ]; then
-        if [ -n "$handler" ]; then
-            options=--server="$handler"
-        fi
-        $(rails_framework) start $options $*
-    else
-        $(rails_framework) server $server $*
-    fi
-}
-
-#
-# Run the rails console.
-#
-rails_console() {
-    # if [ -f Gemfile ] && bundle show pry > /dev/null; then
-    #     bundle exec pry -I. -rconfig/environment
-    # else
-        $(rails_framework) console $*
-    # fi
-}
-
 rails_test_reset() {
   RAILS_ENV=test $(rails_framework) db:drop db:create db:schema:load
 }
 
-alias rrx='rails_server'
-alias rrc='rails_console'
-alias rrdb='$(rails_framework) dbconsole'
-alias rrcs='rails_console --sandbox'
-alias rrr='$(rails_framework) runner'
-alias rrg='$(rails_framework) generate'
-alias rrd='$(rails_framework) destroy'
-alias rrp='$(rails_framework) plugin'
+alias rrx='bin/rails server'
+alias rrc='bin/rails console'
+alias rrdb='bin/rails dbconsole'
+alias rrdbt='RAILS_ENV=test bin/rails dbconsole'
+alias rrcs='bin/rails console --sandbox'
+alias rrr='bin/rails runner'
+alias rrg='bin/rails generate'
 alias rrl='tail -f log/`rails_env`.log'
-alias rry='pry -r ./config/environment'
-alias rrdbt='rails_rake db:test:prepare'
-alias rrtr='rails_test_reset'
+alias rrtr='RAILS_ENV=test bin/rails db:drop db:create db:schema:load'
 
 #
 # Run migrations.
@@ -97,9 +34,9 @@ rrm() {
     local is_integer=$?
 
     if [ "$is_integer" = "0" ]; then
-        rails_rake db:migrate "VERSION=$1"
+        bin/rails db:migrate "VERSION=$1"
     else
-        rails_rake db:migrate $@
+        bin/rails db:migrate $@
     fi
 }
 
@@ -110,7 +47,7 @@ rrm() {
 #
 rrmr() {
     local version=$1; shift
-    rails_rake db:migrate:redo VERSION=`_rails_migration_version $version` $*
+    bin/rails db:migrate:redo VERSION=`_rails_migration_version $version` $*
 }
 
 #
@@ -120,7 +57,7 @@ rrmr() {
 #
 rrmd() {
     local version=$1; shift
-    rails_rake db:migrate:down VERSION=`_rails_migration_version $version` $*
+    bin/rails db:migrate:down VERSION=`_rails_migration_version $version` $*
 }
 
 #
@@ -130,7 +67,7 @@ rrmd() {
 #
 rrmu() {
     local version=$1; shift
-    rails_rake db:migrate:up VERSION=`_rails_migration_version $version` $*
+    bin/rails db:migrate:up VERSION=`_rails_migration_version $version` $*
 }
 
 _rails_migration_version() {
